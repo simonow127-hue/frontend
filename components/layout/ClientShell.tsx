4691bfffe8ee43858afa8f58a97e487063203b84
simonow127-hue <simonow127@gmail.com>
2026-08-12 10:15:23 +0200

    Refactor dynamic imports for components in ClientShell

--- components/layout/ClientShell.tsx
+++ components/layout/ClientShell.tsx
@@ -3,37 +3,66 @@
 import dynamic from "next/dynamic";
 
 const CartDrawer = dynamic(
-  () => import("@/components/cart/CartDrawer"),
-  { ssr: false }
+  () =>
+    import(
+      "@/components/cart/CartDrawer"
+    ),
+  {
+    ssr: false,
+  }
 );
 
 const CheckoutPopup = dynamic(
-  () => import("@/components/checkout/CheckoutPopup"),
-  { ssr: false }
+  () =>
+    import(
+      "@/components/checkout/CheckoutPopup"
+    ),
+  {
+    ssr: false,
+  }
 );
 
 const WhatsAppButton = dynamic(
-  () => import("@/components/ui/WhatsAppButton"),
-  { ssr: false }
+  () =>
+    import(
+      "@/components/ui/WhatsAppButton"
+    ),
+  {
+    ssr: false,
+  }
 );
 
 const PixelManager = dynamic(
-  () => import("@/components/tracking/PixelManager"),
-  { ssr: false }
+  () =>
+    import(
+      "@/components/tracking/PixelManager"
+    ),
+  {
+    ssr: false,
+  }
 );
 
 const SessionInit = dynamic(
-  () => import("@/components/tracking/SessionInit"),
-  { ssr: false }
+  () =>
+    import(
+      "@/components/tracking/SessionInit"
+    ),
+  {
+    ssr: false,
+  }
 );
 
 export default function ClientShell() {
   return (
     <>
       <SessionInit />
+
       <PixelManager />
+
       <CartDrawer />
+
       <CheckoutPopup />
+
       <WhatsAppButton />
     </>
   );
